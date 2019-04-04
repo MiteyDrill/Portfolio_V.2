c84f5510685b86491053e1b453f43bb9e1fb196c
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-03 17:34:35 -0700

    Finished quick Tablet/Mobile view

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 
 anime({
-  targets: 'ul',
-  translateX: 13,
-  duration: 2000
+  targets: '#nav-list ul',
+  translateX: '+= 50%',
+  duration: anime.stagger(150, {start: 500}),
+  easing: 'easeInQuad'
 });
